aa17441c9810830b000457d2085f3d399076dc2f
Shekhov Ismoil <veyxov@gmail.com>
2022-02-28 14:51:42 +0500

    Moved autocommands to lua (requires neovim 0.7

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,12 +1,27 @@
-vim.cmd [[
+local groupname = "WEXOUV"
 
-augroup WEXOUV
-	autocmd!
+-- @param {boolean} clear - optional, defaults to true
+-- @param {string} name - autogroup name
+vim.api.nvim_create_augroup({ name = groupname, clear = true })
 
-	au BufWritePost *.lua so <afile>
-	au BufWritePost plugins.lua PackerCompile
+-- @param {string} name - augroup name
+-- @param {string | table} event - event or events to match against
+-- @param {string | table} pattern - pattern or patterns to match against
+-- @param {string | function} callback - function or string to execute on autocmd
+-- @param {string} command - optional, vimscript command Eg. command = "let g:value_set = v:true"
+-- @param {boolean} once - optional, defaults to false
+vim.api.nvim_create_autocmd {
+    event = "BufWritePost",
+    group = groupname,
+    pattern = "*.lua",
+    callback = function() vim.cmd "so <afile>" end ,
+    once = false
+}
 
-	au TextYankPost * silent! lua vim.highlight.on_yank { higroup = "IncSearch", timeout = 1000 }
-augroup end
-
-]]
+vim.api.nvim_create_autocmd {
+    event = "TextYankPost",
+    group = groupname,
+    pattern = "*",
+    callback = function() vim.highlight.on_yank { higroup = "IncSearch", timeout = 1000 } end ,
+    once = false
+}
