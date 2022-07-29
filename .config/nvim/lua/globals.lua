c3e500a561cee307ee0dbc6456e519ce7d4fa515
Shekhov Ismoil <veyxov@gmail.com>
2022-07-29 09:26:48 +0500

    removed nvim from base config

--- .config/nvim/lua/globals.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-M.Map = function(l, r, m)
-    m = m or "n" -- Default to normal mode
-    vim.keymap.set(m, l, r)
-end
-M.Cmd = function(x) return string.format("<CMD>%s<CR>", x) end
-
-return M
