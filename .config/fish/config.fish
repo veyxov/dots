f208fb0e9a4e6f832eb44f3e85bea1d6876cf554
Shekhov Ismoil <veyxov@gmail.com>
2022-08-29 21:03:22 +0500

    added fish shell

---
+++ .config/fish/config.fish
@@ -0,0 +1,28 @@
+if status is-interactive
+    fish_vi_key_bindings
+end
+
+function fish_mode_prompt
+  switch $fish_bind_mode
+    case default
+      set_color --bold red
+      echo 'N'
+    case insert
+      set_color --bold green
+      echo ''
+    case replace_one
+      set_color --bold green
+      echo 'R'
+    case visual
+      set_color --bold brmagenta
+      echo 'V'
+    case '*'
+      set_color --bold red
+      echo '?'
+  end
+  set_color normal
+end
+
+function kfg
+    lazygit --git-dir=$HOME/.cfg/ --work-tree=$HOME
+end
