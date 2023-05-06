4f6f7f0fbf2b40bae8425db416259f9f54e5e407
Shekhov Ismoil <veyxov@gmail.com>
2023-05-06 15:40:15 +0500

    os: added clipboard persistance

--- start.sh
+++ start.sh
@@ -3,3 +3,5 @@ sudo ~/kanata/target/release/kanata -c ~/.config/keyboard/hands.kbd &
 telegram-desktop &
 firefox &
 dunst &
+wl-paste -t text --watch clipman store &
+wl-paste -p -t text --watch clipman store -P --histpath="~/.local/share/clipman-primary.json" &
