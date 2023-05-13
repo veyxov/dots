005640f3925ba788a4b2453aa1dcbda2a8fddc77
Shekhov Ismoil <veyxov@gmail.com>
2023-05-13 18:14:14 +0500

    wezterm: font chNge

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -81,7 +81,7 @@ return {
     hide_tab_bar_if_only_one_tab = true,
     use_fancy_tab_bar = false,
     window_close_confirmation = 'NeverPrompt',
-    font = wezterm.font("Fira Code"),
+    font = wezterm.font("Source code pro"),
     font_rules = {
         {
             italic = true,
