17f38556cf2bf89bcc5cdaeb8b5b73188c011fe8
Shekhov Ismoil <veyxov@gmail.com>
2023-07-21 10:00:37 +0500

    wezterm: lower the opacity

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -15,7 +15,7 @@ return {
 		brightness = 0.25,
 	},
 	disable_default_key_bindings = true,
-	window_background_opacity = 0.8,
+	window_background_opacity = 0.9,
 	tab_bar_at_bottom = true,
 	quick_select_alphabet = "neiosart",
 	window_padding = {
