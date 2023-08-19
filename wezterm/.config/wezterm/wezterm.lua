2f7bf632f8e8a0d0675f3085cc846e4296df68b7
Shekhov Ismoil <veyxov@gmail.com>
2023-08-19 23:08:11 +0500

    wezterm: added search mode binds

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -73,7 +73,9 @@ return {
         {
             key = 'w',
             mods = 'LEADER',
-            action = wezterm.action.ShowLauncherArgs { flags = 'FUZZY|WORKSPACES' },
+            action = wezterm.action.ShowLauncherArgs {
+                flags = 'FUZZY|WORKSPACES',
+            },
         },
         {
             key = 'p',
@@ -211,6 +213,46 @@ return {
         },
     },
     key_tables = {
+        search_mode = {
+            {
+                key = 'Enter',
+                mods = 'NONE',
+                action = act.CopyMode 'PriorMatch',
+            },
+            { key = 'Escape', mods = 'NONE', action = act.CopyMode 'Close' },
+            { key = 'n', mods = 'CTRL', action = act.CopyMode 'NextMatch' },
+            { key = 'p', mods = 'CTRL', action = act.CopyMode 'PriorMatch' },
+            {
+                key = 'r',
+                mods = 'CTRL',
+                action = act.CopyMode 'CycleMatchType',
+            },
+            {
+                key = 'Backspace',
+                mods = 'CTRL',
+                action = act.CopyMode 'ClearPattern',
+            },
+            {
+                key = 'PageUp',
+                mods = 'NONE',
+                action = act.CopyMode 'PriorMatchPage',
+            },
+            {
+                key = 'PageDown',
+                mods = 'NONE',
+                action = act.CopyMode 'NextMatchPage',
+            },
+            {
+                key = 'UpArrow',
+                mods = 'NONE',
+                action = act.CopyMode 'PriorMatch',
+            },
+            {
+                key = 'DownArrow',
+                mods = 'NONE',
+                action = act.CopyMode 'NextMatch',
+            },
+        },
         copy_mode = {
             {
                 key = 'Enter',
