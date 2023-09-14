185d19aa2cd0a47ff9cae65842a3eb214b3645e5
Shekhov Ismoil <veyxov@gmail.com>
2023-09-14 22:39:47 +0500

    wezterm: nice blue colors

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -11,7 +11,7 @@ end
 return {
     -- cosmetic settings
     font_size = 24,
-    color_scheme = 'dracula',
+    color_scheme ='Icy Dark (base16)',
     hide_tab_bar_if_only_one_tab = true,
     use_fancy_tab_bar = false,
     window_background_opacity = 0.9,
