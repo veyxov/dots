ebacd30266d2095c58b1fde54bbd5c4caa28defb
Shekhov Ismoil <veyxov@gmail.com>
2025-02-22 12:05:51 +0500

    qmk: capsword on oled

--- qmk/qmk_firmware/keyboards/ergohaven/imperial44/keymaps/veyxov/config.h
+++ qmk/qmk_firmware/keyboards/ergohaven/imperial44/keymaps/veyxov/config.h
@@ -1,9 +1,15 @@
 #pragma once
 
-#define TAPPING_TERM 230
+// tapping term
+#define TAPPING_TERM 200
+#define TAPPING_TERM_PER_KEY
 
+// mouse
 #define MOUSEKEY_DELAY       0
 #define MOUSEKEY_INTERVAL    16
 #define MOUSEKEY_WHEEL_DELAY 0
 #define MOUSEKEY_MAX_SPEED   6
 #define MOUSEKEY_TIME_TO_MAX 64
+
+// caps word
+#define CAPS_WORD_IDLE_TIMEOUT 1000
