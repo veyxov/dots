99422c05a43c55e08b29f206f436c4877318fdf5
Shekhov Ismoil <veyxov@gmail.com>
2025-02-22 12:05:51 +0500

    qmk: capsword on oled

--- qmk/qmk_firmware/keyboards/ergohaven/imperial44/keymaps/veyxov/keymap.c
+++ qmk/qmk_firmware/keyboards/ergohaven/imperial44/keymaps/veyxov/keymap.c
@@ -83,10 +83,11 @@ void lnavtd_reset(tap_dance_state_t *state, void *user_data) {
 tap_dance_action_t tap_dance_actions[] = {
     [LNAVTD] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, lnavtd_finished, lnavtd_reset)
 };
+
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case QK_TAP_DANCE ... QK_TAP_DANCE_MAX:
-            return 275;
+        case (TD(LNAVTD)):
+            return TAPPING_TERM - 75;
         default:
             return TAPPING_TERM;
     }
@@ -333,20 +334,10 @@ const key_override_t *key_overrides[] = {
 };
 
 #ifdef OLED_ENABLE
-static void render_logo(void) {
-    static const char PROGMEM qmk_logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
-        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
-    };
-
-    oled_write_P(qmk_logo, false);
-}
-
 bool oled_task_user(void) {
-    render_logo();
-    // Host Keyboard Layer Status
-    oled_write_P(PSTR("Layer: "), false);
+    if (is_caps_word_on()) {
+        oled_write_P(PSTR("CAPSWORD"), false);
+    }
 
     switch (get_highest_layer(layer_state)) {
         case _BASE:
@@ -455,6 +446,7 @@ const uint16_t PROGMEM brah5[] = {KC_SPC, KC_C, COMBO_END};
 const uint16_t PROGMEM brah6[] = {KC_SLSH, S(KC_SLSH), COMBO_END};
 const uint16_t PROGMEM brah7[] = {KC_U, KC_O, COMBO_END};
 const uint16_t PROGMEM brah8[] = {KC_SPC, KC_U, COMBO_END};
+const uint16_t PROGMEM brah9[] = {KC_SPC, KC_O, COMBO_END};
 
 const uint16_t PROGMEM caps[] = {KC_SPC, LTNAV, COMBO_END};
 
@@ -501,7 +493,7 @@ combo_t key_combos[] = {
     COMBO(y, KC_GRV),
     COMBO(zz, KC_GRV),
 
-    COMBO(brah1, KC_0)
+    COMBO(brah1, KC_0),
     COMBO(brah2, KC_2),
     COMBO(brah3, KC_4),
     COMBO(brah4, KC_6),
@@ -510,6 +502,7 @@ combo_t key_combos[] = {
     COMBO(brah6, S(KC_4)),
     COMBO(brah7, KC_EQL),
     COMBO(brah8, S(KC_5)),
+    COMBO(brah9, S(KC_7)),
 
     COMBO(leader, QK_LEAD),
 
