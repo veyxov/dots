bee906c8873b78906f97c62d45c40530457a46ed
Shekhov Ismoil <veyxov@gmail.com>
2026-03-17 17:53:25 +0500

    keyboard: disable bootmagic

--- qmk/qmk_firmware/keyboards/ergohaven/imperial44/config.h
+++ qmk/qmk_firmware/keyboards/ergohaven/imperial44/config.h
@@ -1,10 +1,5 @@
 #pragma once
 
-#define BOOTMAGIC_LITE_ROW          0
-#define BOOTMAGIC_LITE_COLUMN       5
-#define BOOTMAGIC_LITE_ROW_RIGHT    4
-#define BOOTMAGIC_LITE_COLUMN_RIGHT 5
-
 /* Serial settings */
 #define SERIAL_USART_FULL_DUPLEX
 #define SERIAL_USART_TX_PIN GP0
