aef116bba72c73dc3b59f10b59ffb455d181ba95
Shekhov Ismoil <veyxov@gmail.com>
2026-07-20 09:40:25 +0500

    hammerspoon: dim unfocused windows/screens on focus change
    Uses hs.window.highlight overlay mode. Launched alongside aerospace at
    login, same pattern as Telegram.

---
+++ home/dot_hammerspoon/init.lua
@@ -0,0 +1,5 @@
+-- Dim unfocused windows/screens when AeroSpace focus changes
+hs.window.highlight.ui.overlay = true
+hs.window.highlight.ui.overlayColor = { 0, 0, 0, 0.3 }
+hs.window.highlight.ui.frameWidth = 0
+hs.window.highlight.start()
